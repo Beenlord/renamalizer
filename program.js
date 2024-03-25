30ae1f50aee216b2b2d9fa49e11bf2d4a90db7b4
Danila Shuvaev <dshuvaev@art3d.ru>
2024-03-25 16:05:03 +0300

    Finished!

--- program.js
+++ program.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { slugify } = require('transliteration');
 
 
-const rollbackDir = './rollback.json';
+const rollbackDir = path.join(path.dirname(process.argv[0]), '.rollback');
 
 const args = [...process.argv];
 delete args[0];
@@ -11,58 +11,58 @@ delete args[1];
 
 const prom = [];
 
-const rollbackFile = JSON.parse(fs.readFileSync(rollbackDir));
+let rollbackFile = [];
 
-args.forEach((arg) => {
+if (fs.existsSync(rollbackDir)) {
+    rollbackFile = JSON.parse(fs.readFileSync(rollbackDir));
+}
+
+args.forEach(arg => {
     if (path.extname(arg)) {
-        prom.push(new Promise((resolve, reject) => {
             const name = path.basename(arg);
             const slug = slugify(name);
 
-            rollbackFile.forEach((rollback, i) => {
+            toReturn = false;
+            rollbackFile.forEach(rollback => {
                 if (name === rollback.newName) {
-                    try {
-                        resolve(rename(arg, name, rollback.oldName, true));
-                    }
-                    catch(err) {
-                        reject(err)
-                    }
+                    rename(arg, prom, name, rollback.oldName, true);
+                    rollback.delete = true;
+                    toReturn = true;
                 }
             })
-
-            resolve(rename(arg, name, slug));
-            
-        }));
-    }
+            if (toReturn === false) {
+                rename(arg, prom, name, slug);
+            }
+        };
 });
 
 Promise.all(prom)
-    .then((results) => {
-        console.log(results);
+    .then(resolves => {
+        results = resolves.concat(rollbackFile);
         results = results.filter(result => {
-            console.log('result ->', result);
-            result.delete === false;
+            return result.delete === false;
         })
-        results.push(rollbackFile);
 
         fs.writeFileSync(
             rollbackDir,
             JSON.stringify(results),
-            { encoding: "UTF-8"}
+            {encoding: "UTF-8"}
         );
     });
 
-function rename(arg, curName, newName, del) {
-    fs.rename(arg, arg.replace(curName, newName), err => {
-        if (!err) {
-            return({
-                oldName: curName,
-                newName: newName,
-                pathFile: arg,
-                delete: del ?? false,
-            });
-        } else {
-            throw(err);
-        }
-    });
+function rename(arg, promiseArr, curName, newName, del) {
+    promiseArr.push(new Promise((resolve, reject) => {
+        fs.rename(arg, arg.replace(curName, newName), err => {
+            if (!err) {
+                resolve({
+                    oldName: curName,
+                    newName: newName,
+                    pathFile: arg,
+                    delete: del ?? false,
+                });
+            } else {
+                reject(err);
+            }
+        });
+    }))
 }
